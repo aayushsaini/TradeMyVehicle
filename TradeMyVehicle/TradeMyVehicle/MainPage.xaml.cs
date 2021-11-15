34a7ef3863bef9bb80c7e85ad377f64cdea1e497
Aayush Saini <aayush.r98@gmail.com>
2021-11-15 23:45:57 +0530

    Add project files.

---
+++ TradeMyVehicle/TradeMyVehicle/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace TradeMyVehicle
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveBtn_Clicked(object sender, EventArgs e)
+        {
+            Preferences.Set("Username", UserName.Text);
+        }
+
+        private void GetBtn_Clicked(object sender, EventArgs e)
+        {
+            name.Text = Preferences.Get("Username", "");
+        }
+    }
+}
